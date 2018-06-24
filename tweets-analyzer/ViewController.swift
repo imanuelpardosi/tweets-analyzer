6badfc02ca4e9d950aa5cb801a8303f2ba3759d2
Imanuel Pardosi <ipardosi@dyned.com>
2018-06-24 23:51:33 +0700

    Initial commit

---
+++ tweets-analyzer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tweets-analyzer
+//
+//  Created by Mobile Jakarta Team on 24/06/18.
+//  Copyright © 2018 moonshadow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
